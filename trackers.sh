978646e5973d57c68695d65bfb1509277f3d9cc2
Yovcho Yovchev <iam@yovcho.com>
2023-06-21 23:47:13 +0300

    add comments

--- trackers.sh
+++ trackers.sh
@@ -10,6 +10,6 @@ cat all_tmp.txt | sed '/^#/d' | sed '/^# /d' | sed '/^www./d' > all_tmp2.txt
 ## copy all domains with www. prefix
 cat all_tmp2.txt | sed -e "s/\(.*\)/www.\1/" > all_tmp3.txt
 cat all_tmp2.txt all_tmp3.txt > all_domains.txt
-## Remove duplicates records;
+## Remove duplicates records and add 0.0.0.0 prefix for pfblocker;
 awk '!seen[$0]++' "all_domains.txt" | sed -e "s/\(.*\)/0.0.0.0 \1/" > "torrents_trackers.txt";
 rm -rf all_*.txt all-torrent-websites.txt  all-torrent-trackres.txt custom-tracker.txt custom-website.txt;
